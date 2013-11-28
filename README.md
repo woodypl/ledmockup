a9ed8a4dee387d9a4b8b99f451c384556578d8f1
Paweł Drewniak <woody@gigasik.pl>
2013-11-28 09:22:31 +0000

    Initial public revision

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ ledmockup
 =========
 
 A simple Python tool to generate visualisations of LED matrices
+This is a very simple script. Look into the sources to find out what it does.
+
+
+usage: draw.py [-h] [--rows ROWS] [--cols COLS] [--radius RADIUS]
+               [--hdist HDIST] [--vdist VDIST]
+               filename
+
+Project a mockup of a LED matrix display.
+
+positional arguments:
+  filename         filename of the image to display
+
+optional arguments:
+  -h, --help       show this help message and exit
+  --rows ROWS      number of rows (default: 32)
+  --cols COLS      number of columns (default: 32)
+  --radius RADIUS  radius of each LED in pixels (default: 5)
+  --hdist HDIST    horizontal distance between LEDs in pixels (default: 15)
+  --vdist VDIST    vertical distance between LEDs in pixels (default: 15)
---
+++ draw.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+import argparse
+import sys
+import pygame
+from PIL import Image
+
+class Mockup(object):
+
+    def draw_row(self, window, row, matrix, start):
+        for idx in range(self.cols):
+            pygame.draw.circle(window, matrix[idx,row], (start[0]+self.hdist*idx, start[1]), self.radius, 0)
+    
+    def draw_matrix(self, window, matrix, start=(0,0)):
+        for idx in range(self.rows):
+            self.draw_row(window, idx, matrix, (start[0], start[1]+self.vdist*idx))
+
+    def load_image(self):
+        im = Image.open(self.filename)
+        return im.resize((self.rows,self.cols), Image.ANTIALIAS).load()
+
+parser = argparse.ArgumentParser(description='Project a mockup of a LED matrix display.')
+parser.add_argument('filename', type=argparse.FileType('r'),
+                           help='filename of the image to display')
+parser.add_argument('--rows', default=32, type=int,
+        help='number of rows (default: 32)')
+parser.add_argument('--cols', default=32, type=int,
+        help='number of columns (default: 32)')
+parser.add_argument('--radius', default=5, type=int,
+        help='radius of each LED in pixels (default: 5)')
+parser.add_argument('--hdist', default=15, type=int,
+        help='horizontal distance between LEDs in pixels (default: 15)')
+parser.add_argument('--vdist', default=15, type=int,
+        help='vertical distance between LEDs in pixels (default: 15)')
+
+
+
+mockup = Mockup()
+
+parser.parse_args(namespace=mockup)
+
+pygame.init()
+
+#create the screen
+window = pygame.display.set_mode((800, 480), pygame.FULLSCREEN) 
+
+matrix = mockup.load_image()
+mockup.draw_matrix(window, matrix, (mockup.radius, mockup.radius))
+
+#draw it to the screen
+pygame.display.flip() 
+
+#input handling (somewhat boilerplate code):
+while True:
+   for event in pygame.event.get(): 
+      if event.type == pygame.QUIT: 
+          sys.exit(0) 
+      elif event.type == pygame.KEYUP:
+          if event.key == pygame.K_ESCAPE:
+              sys.exit(0)
+      else: 
+          print event 
+   pygame.time.wait(10)
